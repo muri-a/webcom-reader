d40206a3ce3a4a600483b2b691decc9fb0b06424
halftough <halftough29A@gmail.com>
2018-08-29 16:24:25 +0200

    Zooming

--- app/src/main/java/com/example/halftough/webcomreader/activities/ReadChapter/ComicPageView.java
+++ app/src/main/java/com/example/halftough/webcomreader/activities/ReadChapter/ComicPageView.java
@@ -6,16 +6,13 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
-import android.view.WindowManager;
 
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
@@ -35,16 +32,23 @@ public class ComicPageView extends SurfaceView implements Runnable, Target {
 
     private Thread thread;
     private boolean running;
-    private ValueAnimator slideAnimator;
+    private ValueAnimator slideAnimator, xAnimator, yAnimator, wAnimator, hAnimator;
 
     private TouchState touchState = TouchState.EMPTY;
     private boolean zoomed = false;
-    private float startX, startY, startX2, startY2, moveX, moveY;
-    private int imgWidth, imgHeight, padX, padY;
+    private float startX1, startY1, startX2, startY2;
+    private float x1f, y1f, x2f, y2f;
+    private int imgWidth, imgHeight, padX, padY, sPadX, sPadY;
     private int slide;
     private int slideOffset;
-    private static final long backSpeed = 250;
+    private float currentZoom;
 
+    private static final long SLIDE_BACK_SPEED = 300;
+    private static final long SLIDE_NEXT_SPEED = 400;
+    private static final long FIX_ZOOM_SPEED = 80;
+    private static final float MAX_ZOOM_MOD = 2.5f;
+    private static final float MIN_ZOOM_MOD = 0.8f;
+    private float maxZoom, minZoom, noZoom;
 
 
     public ComicPageView(Context context) {
@@ -88,8 +92,7 @@ public class ComicPageView extends SurfaceView implements Runnable, Target {
         super.onTouchEvent(event);
         switch(event.getAction()& MotionEvent.ACTION_MASK){
             case ACTION_DOWN:
-                startX = event.getX();
-                startY = event.getY();
+                saveStartPoint1(event.getX(), event.getY());
                 if(zoomed)
                     touchState = TouchState.MOVE;
                 else
@@ -107,13 +110,12 @@ public class ComicPageView extends SurfaceView implements Runnable, Target {
                 return false;
             case ACTION_POINTER_DOWN:
                 if(touchState==TouchState.DOWN || touchState==TouchState.MOVE){
-                    startX2 = event.getX(1);
-                    startY2 = event.getY(1);
+                    saveStartPoint2(event.getX(1), event.getY(1));
                     touchState = TouchState.ZOOM;
                 }
                 break;
             case ACTION_POINTER_UP:
-                touchState = TouchState.MOVE;
+                endZoom();
                 break;
             case ACTION_MOVE:
                 switch(touchState){
@@ -121,15 +123,15 @@ public class ComicPageView extends SurfaceView implements Runnable, Target {
                         // TODO
                         break;
                     case DOWN:
-                        if( Math.abs(event.getX()-startX) > slideOffset ){
+                        if( Math.abs(event.getX()- startX1) > slideOffset ){
                             touchState = TouchState.SWIPE;
                         }
                         break;
                     case SWIPE:
-                        slide = Math.round(event.getX()-startX);
+                        slide = Math.round(event.getX()- startX1);
                         break;
                     case ZOOM:
-                        // TODO
+                        changeZoom(event.getX(), event.getY(), event.getX(1), event.getY(1));
                         break;
                 }
                 break;
@@ -137,12 +139,64 @@ public class ComicPageView extends SurfaceView implements Runnable, Target {
         return true;
     }
 
+    private void endZoom() {
+        if(currentZoom < noZoom){
+            int newW = Math.round(pageImg.getWidth()*noZoom);
+            int newH = Math.round(pageImg.getHeight()*noZoom);
+            int newX = sPadX;
+            int newY = sPadY;
+
+            animateZoom(newX, newY, newW, newH, (long)((noZoom-currentZoom)/(noZoom-minZoom)*FIX_ZOOM_SPEED));
+            currentZoom = noZoom;
+        }
+        touchState = TouchState.MOVE;
+    }
+
+    private void saveStartPoint1(float x, float y) {
+        startX1 = x;
+        startY1 = y;
+    }
+
+    private void saveStartPoint2(float x, float y) {
+        startX2 = x;
+        startY2 = y;
+
+        // see where we touched on the image, not view (0-begin, 1-end)
+        x1f = (startX1-padX)/imgWidth;
+        y1f = (startY1-padY)/imgHeight;
+        x2f = (x-padX)/imgWidth;
+        y2f = (y-padY)/imgHeight;
+    }
+
+    private void changeZoom(float x1, float y1, float x2, float y2) {
+        // We find position/size such that fxs,fys move to new position
+        float w = (x1-x2)/(x1f-x2f);
+        float h = (y1-y2)/(y1f-y2f);
+        float x = -(x1f*w-x1);
+        float y = -(y1f*h-y1);
+
+        // Fixing aspect ratio
+        // Calculate how much both are being zoomed and take average
+        float zx = w/pageImg.getWidth();
+        float zy = h/pageImg.getHeight();
+        currentZoom = (zx+zy)/2;
+        currentZoom = Math.min(currentZoom,maxZoom); // don't let zoom be bigger than maxZoom
+        currentZoom = Math.max(currentZoom, minZoom); // or smaller than minEditZoom
+
+        imgWidth = Math.round(pageImg.getWidth()*currentZoom);
+        imgHeight = Math.round(pageImg.getHeight()*currentZoom);
+
+        //x,y are calculated for initial transformation, so we need to fix them as well
+        padX = Math.round(x+(w-imgWidth)/2);
+        padY = Math.round(y+(h-imgHeight)/2);
+    }
+
     private void swipeEnd(float x) {
-        slide = Math.round(x-startX);
+        slide = Math.round(x- startX1);
         int a = Math.abs((getWidth()-Math.abs(slide)));
-        final int sign = (int) Math.signum(x - startX);
+        final int sign = (int) Math.signum(x - startX1);
         if(Math.abs(slide) > getWidth()/2){
-            animateSlide(sign * getWidth(), (a*400)/getWidth(), new AnimationObserver() {
+            animateSlide(sign * getWidth(), (a* SLIDE_NEXT_SPEED)/getWidth(), new AnimationObserver() {
                 @Override
                 public void onEnd() {
                     pageImg = null;
@@ -157,7 +211,7 @@ public class ComicPageView extends SurfaceView implements Runnable, Target {
             });
         }
         else {
-            animateSlide(0, Math.abs(slide*300)/getWidth());
+            animateSlide(0, Math.abs(slide* SLIDE_BACK_SPEED)/getWidth());
         }
     }
 
@@ -178,6 +232,48 @@ public class ComicPageView extends SurfaceView implements Runnable, Target {
         slideAnimator.start();
     }
 
+    private void animateZoom(int x, int y, int w, int h, long duration){
+        xAnimator = ValueAnimator.ofInt(padX, x);
+        yAnimator = ValueAnimator.ofInt(padY, y);
+        wAnimator = ValueAnimator.ofInt(imgWidth, w);
+        hAnimator = ValueAnimator.ofInt(imgHeight, h);
+
+        xAnimator.setDuration(duration);
+        yAnimator.setDuration(duration);
+        wAnimator.setDuration(duration);
+        hAnimator.setDuration(duration);
+
+        xAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                padX = (int)animation.getAnimatedValue();
+            }
+        });
+        yAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                padY = (int)animation.getAnimatedValue();
+            }
+        });
+        wAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                imgWidth = (int)animation.getAnimatedValue();
+            }
+        });
+        hAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                imgHeight = (int)animation.getAnimatedValue();
+            }
+        });
+
+        xAnimator.start();
+        yAnimator.start();
+        wAnimator.start();
+        hAnimator.start();
+    }
+
     @Override
     public void run() {
         Canvas canvas;
@@ -189,7 +285,7 @@ public class ComicPageView extends SurfaceView implements Runnable, Target {
                     canvas.save();
                     canvas.drawColor(Color.WHITE);
                     if (pageImg != null) {
-                        Rect src = new Rect(0,0, pageImg.getWidth(),pageImg.getHeight());
+                        //Rect src = new Rect(0,0, pageImg.getWidth(),pageImg.getHeight());
                         RectF des = new RectF(padX+slide, padY, imgWidth+padX+slide, imgHeight+padY);
                         canvas.drawBitmap(pageImg, null, des, paint);
                     }
@@ -217,22 +313,26 @@ public class ComicPageView extends SurfaceView implements Runnable, Target {
 
     private void calcImgSize(){
         if(pageImg != null){
-            float a = (float)(pageImg.getWidth()/pageImg.getHeight());
-            float b = (float)(getWidth()/getHeight());
-            if( a > b ){
+            float imgRatio = (float)(pageImg.getWidth()/pageImg.getHeight());
+            float screenRatio = (float)(getWidth()/getHeight());
+            if( imgRatio > screenRatio ){
                 imgWidth = getWidth();
-                double v = (double)imgWidth/pageImg.getWidth();
-                imgHeight = (int)(pageImg.getHeight()*v);
+                noZoom = (float)imgWidth/pageImg.getWidth();
+                imgHeight = (int)(pageImg.getHeight()*noZoom);
                 padX = 0;
                 padY = (getHeight()-imgHeight)/2;
             }
             else{
                 imgHeight = getHeight();
-                double v = (double)imgHeight/pageImg.getHeight();
-                imgWidth = (int)(pageImg.getWidth()*v);
+                noZoom = (float)imgHeight/pageImg.getHeight();
+                imgWidth = (int)(pageImg.getWidth()*noZoom);
                 padX = (getWidth()-imgWidth)/2;
                 padY = 0;
             }
+            sPadX = padX;
+            sPadY = padY;
+            maxZoom = MAX_ZOOM_MOD*noZoom;
+            minZoom = MIN_ZOOM_MOD*noZoom;
         }
     }
 
